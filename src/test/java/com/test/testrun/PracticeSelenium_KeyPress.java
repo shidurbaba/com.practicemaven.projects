d2574a19a03912a8c5dee318cb68fe6f400f13b7
shidur baba <shidurbaba@gmail.com>
2018-06-30 15:01:38 -0400

    comming branch 2 changes

--- src/test/java/com/test/testrun/PracticeSelenium_KeyPress.java
+++ src/test/java/com/test/testrun/PracticeSelenium_KeyPress.java
@@ -3,7 +3,8 @@ package com.test.testrun;
 public class PracticeSelenium_KeyPress {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// we are practicing selenium pressing key function
+		// you are on mybranch2
 
 	}
 
