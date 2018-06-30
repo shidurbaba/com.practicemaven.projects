fe8c2ce091bf5cdf25b5a49b3d4b769495fbda45
shidur baba <shidurbaba@gmail.com>
2018-06-30 15:12:43 -0400

    practicemaven_v2

--- src/test/java/com/test/testrun/PracticeSelenium_KeyPress.java
+++ src/test/java/com/test/testrun/PracticeSelenium_KeyPress.java
@@ -5,6 +5,7 @@ public class PracticeSelenium_KeyPress {
 	public static void main(String[] args) {
 		// we are practicing selenium pressing key function
 		// you are on mybranch2
+		// you are now on practicemaven.project_v2
 
 	}
 
